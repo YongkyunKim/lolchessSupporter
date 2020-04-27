b32126ccff5cf46a9adef7e7c08900baef14b485
YongkyunKim <47326417+YongkyunKim@users.noreply.github.com>
2020-04-27 19:06:25 +0900

    First Project

---
+++ Lolchess/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lolchess
+{
+    class Program : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new Program());
+
+        }
+    }
+}
